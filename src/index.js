f8c568f12f9861aabf3e126d23438753cb032634
pitambar77 <pitambermajhi33@gmail.com>
2025-08-29 20:24:22 +0530

    bk blog1

--- src/index.js
+++ src/index.js
@@ -39,11 +39,11 @@ import teamSoftRoutes from './routes/TeamRoutes/teamSoftRoutes.js'
 import paymentRoutes from './routes/paymentRoutes.js'
 import blogRoutes from './routes/BlogRoutes/blogRoutes.js'
 
-import path from "path";
-import { fileURLToPath } from "url";
+// import path from "path";
+// import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 dotenv.config()
 const app = express()
@@ -51,7 +51,7 @@ const app = express()
 // Middleware
 app.use(cors())
 app.use(express.json())
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(("uploads")));
 
 
 
